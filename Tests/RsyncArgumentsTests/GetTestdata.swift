e1e564dd455226f5e527736db86352b702e07e03
Thomas Evensen <thomeven@gmail.com>
2024-08-05 13:50:51 +0200

    update

---
+++ Tests/RsyncArgumentsTests/GetTestdata.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  RsyncArguments
+//
+//  Created by Thomas Evensen on 05/08/2024.
+//
+
+import Foundation
+
+struct GetTestdata {
+    let urlSession = URLSession.shared
+    let jsonDecoder = JSONDecoder()
+    
+    private var urlJSON: String = "https://raw.githubusercontent.com/rsyncOSX/RsyncUI/master/versionRsyncUI/versionRsyncUIsonoma.json"
+
+    func getversionsofrsyncuibyurl() async throws -> [DecodeTestdata]? {
+        if let url = URL(string: urlJSON) {
+            let (data, _) = try await urlSession.gettestdata(for: url)
+            return try jsonDecoder.decode([DecodeTestdata].self, from: data)
+        } else {
+            return nil
+        }
+    }
+}
+
+public extension URLSession {
+    func gettestdata(for url: URL) async throws -> (Data, URLResponse) {
+        try await data(from: url)
+    }
+}
