7652fdd73ff7384c30c86a89ec51d69d11132242
Thomas Evensen <thomeven@gmail.com>
2024-08-07 08:48:16 +0200

    Update ArgumentsSynchronize.swift

--- Tests/RsyncArgumentsTests/ArgumentsSynchronize.swift
+++ Tests/RsyncArgumentsTests/ArgumentsSynchronize.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 struct ArgumentsSynchronize {
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // /opt/homebrew/bin/rsync --archive --verbose --compress --delete -e  "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"  --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats --link-dest=/backups/snapshots_JSON/53 /Users/thomas/GitHub/ thomas@raspberrypi:/backups/snapshots_JSON/54
+    
     let nr0 = ["--archive",
                "--verbose",
                "--compress",
@@ -20,6 +23,9 @@ struct ArgumentsSynchronize {
                "--link-dest=/backups/snapshots_JSON/53",
                "/Users/thomas/GitHub/",
                "thomas@raspberrypi:/backups/snapshots_JSON/54"]
+    
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // /opt/homebrew/bin/rsync --archive --verbose --compress --delete -e  "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"  --include-from=/Users/thomas/Documents/includersync/include-list-github.txt --max-size=14K --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats /Users/thomas/Pictures_raw/ thomas@raspberrypi:/backups/Pictures_dopfiles/
 
     let nr1 = ["--archive",
                "--verbose",
@@ -35,6 +41,9 @@ struct ArgumentsSynchronize {
                "/Users/thomas/Pictures_raw/",
                "thomas@raspberrypi:/backups/Pictures_dopfiles/"]
 
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // opt/homebrew/bin/rsync --archive --verbose --compress --delete -e  "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"  --backup --backup-dir=../backup_Documents --dry-run --stats /Users/thomas/Documents/ thomas@raspberrypi:/backups/Documents/
+    
     let nr2 = ["--archive",
                "--verbose",
                "--compress",
@@ -48,6 +57,9 @@ struct ArgumentsSynchronize {
                "/Users/thomas/Documents/",
                "thomas@raspberrypi:/backups/Documents/"]
 
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // /opt/homebrew/bin/rsync --archive --verbose --compress --delete -e  "ssh -i ~/.ssh_local/local -p 3333"  --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats /Users/thomas/GitHub/ thomas@raspberrypi:/backups/GitHub/
+    
     let nr3 = ["--archive",
                "--verbose",
                "--compress",
@@ -60,6 +72,9 @@ struct ArgumentsSynchronize {
                "/Users/thomas/GitHub/",
                "thomas@raspberrypi:/backups/GitHub/"]
 
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // /opt/homebrew/bin/rsync --archive --verbose --delete --dry-run --stats /Users/thomas/Documents/ /Volumes/WesternDigitalBackup/backup/Documents/
+    
     let nr4 = ["--archive",
                "--verbose",
                "--delete",
@@ -68,6 +83,9 @@ struct ArgumentsSynchronize {
                "/Users/thomas/Documents/",
                "/Volumes/WesternDigitalBackup/backup/Documents/"]
 
+    // The full rsync command is, macOS Silicon with rsync from Homebrew:
+    // /opt/homebrew/bin/rsync --archive --verbose --compress --delete -e  "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"  --dry-run --stats  thomas@raspberrypi:/home/thomas/Downloads/ /Users/thomas/Downloads/
+    
     let nr5 = ["--archive",
                "--verbose",
                "--compress",
