5595608573ebd8ade930d8737ad197d4751cef58
Thomas Evensen <thomeven@gmail.com>
2024-08-13 19:22:57 +0200

    Update

--- Tests/RsyncArgumentsTests/RsyncArgumentsTests.swift
+++ Tests/RsyncArgumentsTests/RsyncArgumentsTests.swift
@@ -314,28 +314,3 @@ import Testing
         }
     }
 }
-
-@Suite final class TestCreateSSHkeys {
-    var testconfigurations: [TestSynchronizeConfiguration]?
-
-    @Test func LodaDataCreateSSHKeys() async {
-        let loadtestdata = ReadTestdataFromGitHub()
-        await loadtestdata.getdata()
-        testconfigurations = loadtestdata.testconfigurations
-        if let testconfigurations {
-            // Test for one configuration only, config nr 0
-            guard testconfigurations.count > 0 else { return }
-            let config = testconfigurations[0]
-            let createsshkeys = await CreateSSHkeys(sharedsshport: String(TestSharedReference.shared.sshport ?? -1),
-                sharedsshkeypathandidentityfile: TestSharedReference.shared.sshkeypathandidentityfile
-            )
-
-            let arg3 = await createsshkeys.keypathonly
-            print(arg3 ?? "")
-            let arg4 = await createsshkeys.identityfile
-            print(arg4 ?? "")
-            let arg5 = await createsshkeys.userHomeDirectoryPath
-            print(arg5 ?? "")
-        }
-    }
-}
